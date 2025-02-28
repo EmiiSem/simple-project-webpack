3e9a72cb34b02c101e29d6050e952defecaf3376
EmiiSem <just_dragon29@mail.ru>
2025-02-28 22:39:30 +0300

    Change index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { el, setChildren } from "redom"
+import './main.scss'
+import logo from './logo.svg'
 
 const header = el('header', { class: 'page-header' }, [
-    el('div', { class: 'page-header-text' }, 'Добро пожаловать')
+    el('div', { class: 'page-header-text' }, 'Добро пожаловать на сайт'),
+    el('img', { class: 'page-header-logo', src: logo }),
 ])
 
 setChildren(window.document.body, header)
