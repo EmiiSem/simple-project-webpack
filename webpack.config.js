2f72e306f2b081c5737a63e9dca033f3f9fec97f
EmiiSem <just_dragon29@mail.ru>
2025-02-28 22:15:36 +0300

    Add configuration file builder

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mode: 'production',
+    // Путь до входного JS-файл
+    enrty: './src/index.js',
+    // Размещение настроек файла после сборки
+    output: {
+        filename: 'index.js'
+    }
+}
