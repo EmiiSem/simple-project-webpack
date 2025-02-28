00a12a9e5c8656d5aa9604d4b22957bbdb70aa4e
EmiiSem <just_dragon29@mail.ru>
2025-02-28 22:34:18 +0300

    Change config file webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,29 @@ module.exports = {
     // Размещение настроек файла после сборки
     output: {
         filename: 'index.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+            },
+        ]
     }
 }
